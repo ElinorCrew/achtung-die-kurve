6e7b06aed11fc0fcbb689720517a99a33f90d42c
teodoran <teodoran@stud.ntnu.no>
2015-01-24 19:44:54 +0100

    Added path d string output option

--- client/path.js
+++ client/path.js
@@ -1,16 +1,41 @@
-var path = function (input_coords) {
+var path = function (inputCoords) {
     'use strict';
-    var p = {},
-        coords = [];
 
-    if (input_coords) {
-        coords = input_coords;
-    }
+    var p = {},
+        coords = inputCoords || [],
+
+        first = function (array) {
+            return array.slice(0, 1)[0];
+        },
+
+        rest = function (array) {
+            return array.slice(1, array.length);
+        },
+
+        d = function (array, command) {
+            if (!array[0]) {
+                return '';
+            }
+
+            var firstCoord = first(array),
+                dx = firstCoord[0],
+                dy = firstCoord[1],
+                arg = command || 'M';
+
+            return arg
+                + dx + ' '
+                + dy
+                + d(rest(array), ' L');
+        };
 
     p.push = function (coord) {
         return path(
             coords.concat([coord])
         );
+    };
+
+    p.string = function () {
+        return pathString(coords);
     };
 
     p.value = function () {
